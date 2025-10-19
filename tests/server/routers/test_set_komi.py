407c603a45c915663faa66a43a1c57af7abf7436
ReiRev <reirev2913@gmail.com>
2025-10-19 06:20:16 +0000

    Refactor test_set_komi to use a variable for komi value and add verification for the retrieved komi

--- tests/server/routers/test_set_komi.py
+++ tests/server/routers/test_set_komi.py
@@ -1,10 +1,14 @@
 def test_set_komi(client, session_id):
-    res = client.post(f"/{session_id}/komi", json={"value": 6.5})
-    assert res.status_code == 200
+    komi = 10
 
+    res = client.post(f"/{session_id}/komi", json={"value": komi})
+    assert res.status_code == 200
     data = res.json()
     assert "detail" in data
     assert isinstance(data["detail"], str)
+
+    res = client.get(f"/{session_id}/komi")
+    assert res.json()["komi"] == komi
 
 
 def test_set_komi_invalid_session(client, invalid_session_id):
